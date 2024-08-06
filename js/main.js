b8427bbad33a98ac9b245d2113c53c8d05e5a980
MrKarimov01 <108998528+MrKarimov01@users.noreply.github.com>
2024-08-06 08:06:53 +0500

    nav line hash

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 let lines = document.querySelectorAll(".line");
 let li_items = document.querySelectorAll(".nav_li_items");
+let hrefLoc = window.location.hash.substring(1);
 // let count = sessionStorage.setItem("link");
 function change(e){
-    sessionStorage.setItem("link","true")
     lines.forEach((item)=>item.classList.remove("line_active"))
     
     if(e.querySelector('.line')){
         e.querySelector('.line').classList.add("line_active")
-        sessionStorage.setItem("navKey",e.querySelector('a').textContent)
+        
         
     }  
 }
+li_items.forEach(item=>{
+    let ahref=  item.querySelector("a")
+    let linein = item.querySelector('.line')
+    if(hrefLoc.toLowerCase() == ahref.textContent.toLocaleLowerCase()){
 
-li_items.forEach(i=>{
-    let store = sessionStorage.getItem("navKey")
-
-    if(store == i.querySelector("a").textContent){
-        i.querySelector(".line").classList.add("line_active")
+        console.log(linein.setAttribute("class","line line_active"));
+        
     }
+    
 })
+
+
